edd1903c5feace82abb4ba7940fa71633aad8a04
Min1600 <aminhassan00@gmail.com>
2024-07-08 22:10:22 +0100

    adding calculations

--- Index.js
+++ Index.js
@@ -4,19 +4,20 @@
 
 
 
-function add(){
-
+function add(a, b){
+return a + b
 }
 
-function subtract(){
-
+function subtract(a, b){
+    return a - b 
 }
 
-function multiply(){
-
+function multiply(a, b){
+return a*b
 }
 
-function divide(){
-
+function divide(a,b ){
+return a/b
 }
+
 
