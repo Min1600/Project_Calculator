d2dde7756f7e8e2cfbeb14d1b8c855317c644b53
Min1600 <aminhassan00@gmail.com>
2024-07-10 19:20:33 +0100

    temp save

--- Index.js
+++ Index.js
@@ -1,7 +1,16 @@
+const button = document.querySelector(".btn")
+const screen = document.querySelector(".screen")
+const div = document.querySelector("div")
+let firsNumber;
+let secondNumber;
+let operator;
 
+function operate(){
+    
+}
+function display(){
 
-
-
+}
 
 
 function add(a, b){
@@ -19,5 +28,15 @@ return a*b
 function divide(a,b ){
 return a/b
 }
+
+
+div.addEventListener("click", (event) => {
+    if(event.target.tagName === "BUTTON"){
+        let display = document.createElement("div")
+        display.innerText = event.target.innerText
+        screen.appendChild(display)
+    }
+})
+
 
 
