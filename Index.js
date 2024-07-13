a689dea0fe0da0074c831eabcd6ad31984825c07
Min1600 <aminhassan00@gmail.com>
2024-07-13 11:56:56 +0100

    temp save

--- Index.js
+++ Index.js
@@ -3,13 +3,12 @@ const screen = document.querySelector(".screen")
 const div = document.querySelector("div")
 const clear = document.querySelector("#C")
 const equal = document.querySelector("#equals")
-const first = document.querySelector(".first")
-const operate = document.querySelector(".operator")
-const second = document.querySelector(".second")
-let firsNumber;
+let first = document.querySelector(".first")
+let operate = document.querySelector(".operator")
+let second = document.querySelector(".second")
+let firstNumber;
+let operator = {};
 let secondNumber;
-let operator;
-
 
 function reset(){
     first.innerText = ""
@@ -17,6 +16,12 @@ function reset(){
     operate.innerText = ""
 }
 
+function calculation(){
+let calc
+console.log(Number(firstNumber))
+console.log(operator.val)
+console.log(Number(secondNumber))
+}
 function add(a, b){
 return a + b
 }
@@ -34,28 +39,25 @@ return a/b
 }
 
 div.addEventListener("click", (event) => {
-    if(event.target.tagName === "BUTTON" && event.target.id !== "C" && event.target.id !== "equals"){
-        
-        if(event.target.id === "7" || event.target.id === "8" ||event.target.id === "9" ||
-            event.target.id === "4" ||event.target.id === "5" ||event.target.id === "6" ||
-            event.target.id === "1" ||event.target.id === "2" ||event.target.id === "3" ||event.target.id === "0"
-        ){
-        let firstNumber = document.querySelector(".first")
-        firstNumber.innerText += event.target.innerText
-        
-    }
-    else if(event.target.id === "/" || event.target.id === "x" || event.target.id === "-" || event.target.id === "+")
-    {
-        let operator = document.querySelector(".operator")
-        operator.innerText = event.target.innerText
-    }
-        else if(event.target.id === "7" || event.target.id === "8" ||event.target.id === "9" ||
-            event.target.id === "4" ||event.target.id === "5" ||event.target.id === "6" ||
-            event.target.id === "1" ||event.target.id === "2" ||event.target.id === "3" ||event.target.id === "0"
-        ){
-        let secondNumber = document.querySelector(".second")
-        secondNumber.innerText += event.target.innerText}
-    }
+
+if(event.target.tagName === "BUTTON" && event.target.id !== "C"){
+
+if(event.target.id !== "/" && event.target.id !== "*" && event.target.id !== "-" && event.target.id !== "+" && event.target.id !== "equals" && operate.innerText === ""){
+first.innerText += event.target.innerText
+firstNumber = first.innerText
+
+}else if(operate.innerText !== "" && event.target.id !== "/" && event.target.id !== "*" && event.target.id !== "-" && event.target.id !== "+" && event.target.id !== "equals"){
+    second.innerText += event.target.innerText
+    secondNumber = second.innerText
+
+}else if(second.innerText === "" && event.target.id !== "equals"){
+    operate.innerText = event.target.innerText;
+    operator.val = operate.innerText
+}else if(event.target.id === "equals" || event.target.id === "/" || event.target.id === "*" || event.target.id === "+" || event.target.id === "-" ){
+calculation()
+}
+
+}
 }
 )
 
